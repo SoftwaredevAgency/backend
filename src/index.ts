fd30b36895c443624894aee4d3cbbb4c61037a4a
amschel99 <6290.2020@students.ku.ac.ke>
2024-04-24 09:36:04 +0300

    setup server

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { Request, Response } from "express";
+import { connectDb } from "./db/config";
+import http from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+export const app = express();
+const httpServer = http.createServer(app);
+
+export const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  socket.emit("newConnection", { message: "a new client connected" });
+});
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(`app is working`);
+});
+const connectToDB = async () => {
+  try {
+    if (process.env.NODE_ENV === "test") {
+      return null;
+    }
+    if (process.env.NODE_ENV === "remote") {
+      await connectDb(
+        process.env.REMOTE_MONGO ??
+          "mongodb+srv://nichlabs:4JsAR0yT34fUO1Iq@nichlabsapp.thoorlb.mongodb.net/?retryWrites=true&w=majority&appName=NichlabsApp"
+      );
+      console.log("DB connected successfully! yes");
+      httpServer.listen(process.env.PORT, () => {
+        console.log(`Server up and running on ${process.env.PORT}`);
+      });
+      return;
+    }
+
+    await connectDb(
+      process.env.MONGO ??
+        "mongodb+srv://nichlabs:4JsAR0yT34fUO1Iq@nichlabsapp.thoorlb.mongodb.net/?retryWrites=true&w=majority&appName=NichlabsApp"
+    );
+    console.log("DB connected successfully");
+    const used = process.memoryUsage();
+    console.log(`Heap memory used ${JSON.stringify(used)}`);
+    httpServer.listen(process.env.PORT, () => {
+      console.log(`Server up and running on ${process.env.PORT} `);
+    });
+  } catch (err: any) {
+    console.log(
+      `Connection on ${process.env.REMOTE_MONGO} failed: ${err.message}`
+    );
+    console.log("Retrying connection...");
+
+    setTimeout(connectToDB, 5000);
+  }
+};
+
+connectToDB();
